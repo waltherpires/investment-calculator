733519a38789920fa35592bf8134c8462d60577b
Walther Oliveira <waltherpires@hotmail.com>
2024-11-06 21:50:25 -0300

    Header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from '../assets/investment-calculator-logo.png'
+
+export default function Header(){
+    return (
+        <header id="header">
+            <img src={Logo} alt="logo" />
+            <h1>React Investment Calculator</h1>
+        </header>
+    );
+}
