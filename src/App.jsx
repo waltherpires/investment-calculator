ab0c6d728e8cfec15690ff0b4d98809c383dc99b
Walther Oliveira <waltherpires@hotmail.com>
2024-11-07 00:22:06 -0300

    displaying results

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
+import { useState } from 'react';
 
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <main>
-        <Header />
-        <UserInput />
-    </main>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
