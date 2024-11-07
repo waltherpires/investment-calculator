e802f176edd6aee5b857109dd9766cbf0cc6c36f
Walther Oliveira <waltherpires@hotmail.com>
2024-11-06 22:43:26 -0300

    UserInput added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+
 
 function App() {
   return (
-    <Header />
+    <main>
+        <Header />
+        <UserInput />
+    </main>
   )
 }
 
